67917f95b50b99ebc47393822beb6eb99934415e
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-11-03 10:05:18 +0000

    Todas las funcionalidades terminadas

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -7,12 +7,15 @@ const TodoList = () => {
     const [todoCont, setTodoCont] = useState(0)
     let localList = [...todoList]
 
-    // declaro una funcion para escuchar la tecla enter y meter dentro del array el valor de mi input
+    // utilizo el hook UseEffect para actualizar mi lista de todos y mi contador de todos
     useEffect(() => {
         getTodos()
         setTodoCont(localList.length)
+
     })
 
+    //declaro mi funcion para leer los valores escritos por el usuario, luego capturo la tecla enter con un if
+    //creo un objeto con los valores si el input no esta vacio para luego hacer un PUT request
     const enterItem = async (e) => {
         try {
 
@@ -33,14 +36,13 @@ const TodoList = () => {
                     body: JSON.stringify(localList)
                 });
                 setInput("")
-                console.log(todoList)
 
             }
         } catch (e) {
             console.log("enterItem function ERROR === ", e)
         }
     }
-    //Declaro mi funcion de borrar las tareas mediante el metodo filter retorno un nuevo array sin el elemento seleccionado
+    //Declaro mi funcion de borrar las tareas mediante el metodo filter retorno un nuevo array sin el elemento seleccionado y luego hago un put con el array retornado
     const deleteTask = async (ele) => {
         try {
             const newTodoList = todoList.filter((task) => { return ele !== task.id })
@@ -60,7 +62,7 @@ const TodoList = () => {
             console.log("deleteUser function ERROR === ", e)
         }
     }
-
+    // hago un GET request para convertir la respuesta en mi estado local
     const getTodos = async () => {
         try {
 
@@ -76,7 +78,7 @@ const TodoList = () => {
             location.reload(true)
         }
     }
-
+    // declaro mi funcion y deleteAll que borra todos las tareas
     const deleteAll = async () => {
         try {
             await fetch("https://playground.4geeks.com/apis/fake/todos/user/estarlin", { method: 'DELETE' })
@@ -84,7 +86,7 @@ const TodoList = () => {
             console.log("deleteAll function ERROR=== ", e)
         }
     }
-
+    //declaro mi funcion que hace un POST request y crear el usuario en caso de ser borrado
     const createUser = async () => {
         try {
             await fetch("https://playground.4geeks.com/apis/fake/todos/user/estarlin", {
@@ -107,20 +109,20 @@ const TodoList = () => {
                 <div className="todoListTitle">
                     <h1>To Do List</h1>
                 </div>
-                <input type="text" placeholder="What needs to be done?" className="todoInput col-12" onKeyDown={enterItem} value={input} onChange={(event) => setInput(event.target.value)} />
+                <input type="text" placeholder="What needs to be done?" className="todoInput col-12 form-control" onKeyDown={enterItem} value={input} onChange={(event) => setInput(event.target.value)} />
+
                 <button className="btn btn-danger w-100 my-2 p-2 fs-5" onClick={() => deleteAll()} >Delete All Tasks!!</button>
+
+                <div className={todoCont == 1 ? "noTaskMessage" : "hiden"}>
+                    <h1>{todoCont == 1 ? "No tasks, add a task" : ""}</h1>
+                </div>
 
                 <div className="row">
                     <ul >
-
-                        <div className={todoCont == 5 ? "noTaskMessage" : "hiden"}><h1>{todoCont == 5 ? "No tasks, add a task" : ""}</h1></div>
-
-                        {todoList.map((ele, index) => <li key={index} className="task col-11">{ele.label} <div className="deleteIcon " onClick={() => deleteTask(ele.id)}>X</div></li>)}
-
+                        {todoList.map((ele, index) => <li key={index} className={index > 0 ? "task col-11" : "hide"}>{ele.label} <div className="deleteIcon " onClick={() => deleteTask(ele.id)}>X</div></li>)}
                     </ul>
                 </div>
-                <div className="todosCont">{todoCont} Item Left</div>
-
+                <div className="todosCont">{todoCont - 1} Item Left</div>
             </div>
         </div>
     );
